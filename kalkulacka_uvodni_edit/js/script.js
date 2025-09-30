bd14def889d138886b383e152ff4f1619b785672
Vít Legát <102857032+Orosky@users.noreply.github.com>
2025-09-30 15:12:57 +0200

    Add files via upload

---
+++ kalkulacka_uvodni_edit/js/script.js
@@ -0,0 +1,27 @@
+function secti() {
+  let hodnota1 = parseInt(document.getElementById("id_prvni").value);
+  let hodnota2 = parseInt(document.getElementById("id_druhy").value);
+  let vysledek = hodnota1 + hodnota2;
+  document.getElementById("id_vysledek").value = vysledek;
+}
+
+function odecti() {
+  let hodnota1 = parseInt(document.getElementById("id_prvni").value);
+  let hodnota2 = parseInt(document.getElementById("id_druhy").value);
+  let vysledek = hodnota1 - hodnota2;
+  document.getElementById("id_vysledek").value = vysledek;
+}
+
+function vynasob() {
+  let hodnota1 = parseInt(document.getElementById("id_prvni").value);
+  let hodnota2 = parseInt(document.getElementById("id_druhy").value);
+  let vysledek = hodnota1 * hodnota2;
+  document.getElementById("id_vysledek").value = vysledek;
+}
+
+function vydel() {
+  let hodnota1 = parseInt(document.getElementById("id_prvni").value);
+  let hodnota2 = parseInt(document.getElementById("id_druhy").value);
+  let vysledek = hodnota1 / hodnota2;
+  document.getElementById("id_vysledek").value = vysledek;
+}
